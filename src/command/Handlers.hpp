4fe2468f13eddc47ba81be1137810770610c52c9
lsulzbac <lsr.jupi@gmail.com>
2025-02-18 20:05:48 +0100

    update TODOs

--- src/command/Handlers.hpp
+++ src/command/Handlers.hpp
@@ -6,12 +6,6 @@
 
 /* TODO:
 1. OPER - @David
-
-//Metodos do operador para implementar (?)
-KILL -> eliminar um usuario do servidor :D @Lucas
-DIE == VALGRIND -> ja implementado, so mudar nome @Lucas
-
-Define maxLEN for topic @Lucas
 */
 
 void handleCap(User &user, Server &server, const std::vector<std::string> &args);
